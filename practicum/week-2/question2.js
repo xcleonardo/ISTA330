dabda6b54966fdcce1fb98a845246006ef83ee92
xcleonardo <echeverl@email.arizona.edu>
2021-02-11 00:28:56 -0700

    Update question2.js

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -8,18 +8,15 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
          Because we can give 5 of the extraCookies to the first kid and the remaining 3 to the second kid
          and therfore each of the three kids will get 8 cookies.
  */
-
 var canGetEqualCookies = function(cookies, extraCookies) {
-
-
-        var cookieMax = Math.max.apply(null, cookies);
-        for (i = 0; i < cookies.length; i++) {
-           while (cookies[i] != cookieMax && extraCookies != 0) {
-                   cookies[i] += 1;
-                   extraCookies -= 1;
-           }
-         }
-         const allEqual = arr => arr.every( v => v === arr[0] );
-         return allEqual(cookies);
-   
-    };
+        var sum = 0;
+        for (var i = 0; i < cookies.length; i++){
+                sum += cookies[i];
+        }
+        sum += extraCookies;
+        if(sum % cookies.length == 0){
+                return true;
+        } else {
+                return false;
+        }
+}; 
